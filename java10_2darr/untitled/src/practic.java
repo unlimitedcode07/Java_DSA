cee3243c5a9aefebf0b4536e454067e4b00539c2
Your Name <kwwhiuwhefk@gmail.com>
2026-04-21 10:15:54 +0530

    practic recet for arr2D

--- java10_2darr/untitled/src/practic.java
+++ java10_2darr/untitled/src/practic.java
@@ -3,12 +3,8 @@ import java.util.*;
 
 public class practic {
 
-    public static  boolean searchMatrix(int[][] arr, int t) {
 
-    }
     public static void main(String[] args) {
-        int [][] arr = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
-        int a = 0;
-        System.out.println(searchMatrix(arr,a));
+
     }
 }
