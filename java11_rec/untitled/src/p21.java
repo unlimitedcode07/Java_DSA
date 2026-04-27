d0477ff722e209c1994d25291de17a4538eb4f07
Your Name <kwwhiuwhefk@gmail.com>
2026-04-27 10:14:46 +0530

    at rec p21 add q of power set

--- java11_rec/untitled/src/p21.java
+++ java11_rec/untitled/src/p21.java
@@ -1,3 +1,32 @@
+//Power Set
+//        Difficulty: MediumAccuracy: 43.3%Submissions: 119K+Points: 4
+//        Given a string s of length n, find all the possible non-empty subsequences of the string s in lexicographically-sorted order.
+//
+//        Example 1:
+//
+//        Input :
+//        s = "abc"
+//        Output:
+//        a ab abc ac b bc c
+//        Explanation :
+//        There are a total 7 number of subsequences possible for the given string, and they are mentioned above in lexicographically sorted order.
+//        Example 2:
+//
+//        Input:
+//        s = "aa"
+//        Output:
+//        a a aa
+//        Explanation :
+//        There are a total 3 number of subsequences possible for the given string, and they are mentioned above in lexicographically sorted order.
+//        Your Task:
+//        You don't need to read input or print anything. Your task is to complete the function AllPossibleStrings() which takes a string s as the
+//        input parameter and returns a list of all possible subsequences (non-empty) that can be formed from s in lexicographically sorted order.
+//
+
+
+
+
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
--- java11_rec/untitled/src/practic.java
+++ java11_rec/untitled/src/practic.java
@@ -1,10 +1,29 @@
 
-import javax.naming.PartialResultException;
+
 import java.util.*;
 
 public class practic {
+    public static List<String> AllPossibleStrings(String s) {
+     List<String > arr = new ArrayList<>();
+     int i = 0;
+     String curent = "";
+     arr = ans(arr,s,i,curent) ;
+     Collections.sort(arr);
+     arr.remove(0);
+    return arr;
+    }
     public static void main(String[] args) {
+      String op = "abc";
+      System.out.println(AllPossibleStrings(op));
+    }
+    public static List<String> ans(List arr , String s, int i, String c) {
+      if(i >=3){
+          arr.add(c);
+          return arr;
+      }
 
+      ans(arr,s,i+1,c);
+      return ans(arr,s,i+1,c+s.charAt(i));
     }
 
 }
