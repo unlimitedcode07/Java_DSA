75b9f747df5aecde380a66cd9b7ab5674e4bf187
Your Name <kwwhiuwhefk@gmail.com>
2026-05-06 23:10:01 +0530

    complet linkedlist operation  from [DSA In Java | Linked List | Java in One Shot | Linked List in Java] till 3:13:30

--- java15_linkdlist/linkdlist/src/linkdlist2/p1.java
+++ java15_linkdlist/linkdlist/src/linkdlist2/p1.java
@@ -12,8 +12,10 @@ class Node{
 class linkdlist{  // user define data structure
    Node head; // default null
    Node tail; // default null
+   int size;
 
-    void addlist(int val){
+
+   void addlist(int val){
         Node temp = new Node(val);
         if(head == null) head = tail = temp;
         //this will try to add first node cause at start tail and head are null
@@ -22,6 +24,7 @@ class linkdlist{  // user define data structure
             tail.next = temp;
             tail = temp;
         }
+        size++;
     }
    void displayR(){
         if(head == null) return ;
@@ -34,17 +37,17 @@ class linkdlist{  // user define data structure
        System.out.println();
    }
 
-    public void addhead(int i) {
+    void addhead(int i) {
         Node temp = new Node(i);
         if(head == null) head = tail = temp;
         else{
             temp.next = head;
             head = temp;
         }
-
+  size++;
     }
 
-    public void deleteAtSt() {
+    void deleteAtSt() {
         if(tail == null) {
             System.out.println("list is empty");
             return;
@@ -53,6 +56,84 @@ class linkdlist{  // user define data structure
             head = temp;
             // or head = head.next {easy}
         if(temp == null) tail = null;
+        size--;
+    }
+    boolean search(int val){
+        Node temp = head;
+        while(temp!=null){
+            if(temp.val == val) return true;
+            temp = temp.next;
+        }
+        return false;
+    }
+
+    void insertAtAny(int idx, int val) {
+        if(idx < 0 || idx > size){
+            System.out.println("Invalid index");
+            return;
+        }
+
+        Node newNode = new Node(val);
+
+        // insert at head
+        if(idx == 0){
+            newNode.next = head;
+            head = newNode;
+            if(size == 0) tail = newNode;
+        }
+        else{
+            Node temp = head;
+
+            for(int i = 1; i < idx; i++){
+                temp = temp.next;
+            }
+
+            newNode.next = temp.next;
+            temp.next = newNode;
+
+            // update tail if inserted at end
+            if(idx == size){
+                tail = newNode;
+            }
+        }
+
+        size++;
+    }
+
+    void delete(int idx) {
+        if(idx < 0 || idx >= size){
+            System.out.println("Invalid index");
+            return;
+        }
+
+        // delete head
+        if(idx == 0){
+            head = head.next;
+            size--;
+
+            // if list becomes empty
+            if(size == 0){
+                tail = null;
+            }
+            return;
+        }
+
+        Node temp = head;
+
+        // reach (idx - 1)
+        for(int i = 1; i < idx; i++){
+            temp = temp.next;
+        }
+
+        // delete node
+        temp.next = temp.next.next;
+
+        // update tail if last element deleted
+        if(idx == size - 1){
+            tail = temp;
+        }
+
+        size--;
     }
 }
 //  h = head
@@ -92,6 +173,16 @@ public class p1 {
         a.displayR();
         //87 ->90 ->3 ->4 ->5 ->6 ->null
         a.deleteAtSt();
+        a.displayR();
+        //90 ->3 ->4 ->5 ->6 ->null
+        System.out.println(a.size);
+        //5
+        System.out.println(a.search(6));
+        // true
+        a.insertAtAny(0,78);
+        a.displayR();
+        //78 ->90 ->3 ->4 ->5 ->6 ->null
+        a.delete(0);
         a.displayR();
         //90 ->3 ->4 ->5 ->6 ->null
     }
